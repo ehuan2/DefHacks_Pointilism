83a5314bfd3289c382a36340a8fafb9bb9de32c0
ehuan2 <eric.haoran.huang@gmail.com>
2020-06-20 17:00:11 -0400

    Added in the cookies for flask

--- flask_app/main/routes.py
+++ flask_app/main/routes.py
@@ -1,34 +1,72 @@
-from flask import render_template, Blueprint, request, redirect, url_for
+from flask import render_template, Blueprint, request, redirect, url_for, make_response
 from flask_app.main.forms import ImageInputForm, LargerImageForm
 from flask_app.main.utils import save_picture, generate_image
 from PIL import Image
 import os
+import secrets
+
+cookies_dict = {}
 
 main = Blueprint('main', __name__)
+
 
 @main.route("/", methods=['GET', 'POST'])
 def index():
 
+    # the cookies stuff!
+    cookie = request.cookies.get('userID')
+
+    # creates a cookie if it doesn't exist
+    if not cookie:
+        cookie = secrets.token_hex(8)
+        cookies_dict[cookie] = []
+
+    if not cookies_dict.get(cookie):
+        cookies_dict[cookie] = []
+
+    # the different forms, smaller image and larger
     image_form = ImageInputForm()
     larger_image_form = LargerImageForm()
 
     if image_form.validate_on_submit():
+
         if image_form.image.data:
-            save_picture(image_form.image.data, output_size=(8, 8))
+        
+            # adds in an image based on the cookie
+            cookies_dict[cookie].append(save_picture(
+                image_form.image.data, output_size=(8, 8)))
 
-    images = [img_path for img_path in os.listdir("./flask_app/static/images")]
+    # gets the images
+    images = cookies_dict.get(cookie)
 
-    return render_template("main.html", image_form=image_form, larger_image_form = larger_image_form, images = images)
+    # prepares a response
+    resp = make_response(render_template("main.html", image_form=image_form,
+                                         larger_image_form=larger_image_form, images=images))
+
+    resp.set_cookie('userID', cookie)
+    return resp
 
 
-@main.route("/generate_image", methods = ['POST'])
+@main.route("/generate_image", methods=['POST'])
 def generate_image_route():
 
     if request.method == 'POST':
         img = request.files.get("image")
 
         if img:
-            larger_image = Image.open(request.files.get("image").stream)
-            generate_image(larger_image=larger_image)
+            larger_image = request.files.get("image").stream
+
+            cookie = request.cookies.get('userID')
+
+            # creates a cookie if it doesn't exist
+            if not cookie:
+                cookie = secrets.token_hex(8)
+
+
+            if not cookies_dict.get(cookie):
+                cookies_dict[cookie] = []
+
+            cookies_dict[cookie].append(generate_image(larger_image))
+
 
     return redirect(url_for('main.index'))
--- flask_app/main/utils.py
+++ flask_app/main/utils.py
@@ -1,15 +1,32 @@
 from flask import current_app
 from PIL import Image
 import os
+import secrets
+
 
 def save_picture(form_picture, output_size):
-    
-    image_path = os.path.join(current_app.root_path, 'static/images', form_picture.filename)
-    
+
+    filename = form_picture.filename
+
+    image_path = os.path.join(current_app.root_path, 'static/images', filename)
+
     img = Image.open(form_picture)
+
     img.thumbnail(output_size)
+
     img.save(image_path)
+
+    return filename
+
 
 
 def generate_image(larger_image):
-    larger_image.show()
+    filename = secrets.token_hex(8)
+
+    image_path = os.path.join(current_app.root_path, 'static/images', filename + ".png")
+
+    img = Image.open(larger_image)
+
+    img.save(image_path)
+
+    return filename + ".png"
