21d8659d28cbd12fa1ae146d391afd715908632f
ehuan2 <eric.haoran.huang@gmail.com>
2020-06-20 23:03:18 -0400

    Adapted the image processing for flask

--- image_processing/main.py
+++ image_processing/main.py
@@ -6,192 +6,141 @@ import cv2
 import math
 import urllib
 
-size_8 = (8, 8)
-size_256 = (256, 256)
 
-pictureRGB = {}
-faceAverages = []
-closestImages = []
+def get_processed_image(larger_image, images_8 = []):
 
-# Scripts to shrink down images to smaller size
+    if larger_image:
 
-path1 = os.path.normpath(".")
+        # the larger image is the one that will be processed later
+        # the images 8 is the group of images that will be passed in (or can simply be used through db and/or the file system)
+        # images_8 is going to be all the names
 
-for i in os.listdir(path1):
-    if i.endswith(".png") and str(i) == "face.png":
-        print("ah yes")
-        image = Image.open(i)
-        fn, fext = os.path.splitext(i)
-        new_image = image.resize(size_256)
-        new_image.save("./finalImage/face.png")
+        size_8 = (8, 8)
+        larger_size = (256, 256)
 
+        pictureRGB = {}
+        faceAverages = []
+        closestImages = []
 
-for i in os.listdir('./images'):
-    if i.endswith(".jpg"):
-        image = Image.open("./images/"+i)
-        fn, fext = os.path.splitext(i)
-        new_image = image.resize(size_8)
-        new_image.save("./8images/{}.png".format(fn))
+        # Scripts to shrink down images to smaller size
 
+        path = "./image_processing"
 
-def getAverageRGBN(image):
-    """
-    Given PIL Image, return average value of color as (r, g, b)
-    """
-    # get image as numpy array
-    im = np.array(image)
-    # get shape
-    w, h, d = im.shape
-    # change shape
-    im.shape = (w*h, d)
-    # get average
-    return tuple(np.average(im, axis=0))
+        def getAverageRGBN(image):
+            """
+            Given PIL Image, return average value of color as (r, g, b)
+            """
+            # get image as numpy array
+            im = np.array(image)
+            # get shape
+            w, h, d = im.shape
+            # change shape
+            im.shape = (w*h, d)
+            # get average
+            return tuple(np.average(im, axis=0))
+
+        # Average pixels in all images and save it to dict
+        for imagename in os.listdir(f'{path}/8images'):
+            image = Image.open(f"{path}/8images/"+imagename)
+            pictureRGB[imagename] = getAverageRGBN(image)
 
 
-# Average pixels in all images and save it to dict
-for imagename in os.listdir('./8images'):
-    image = Image.open("./8images/"+imagename)
-    pictureRGB[imagename] = getAverageRGBN(image)
+        im = np.array(larger_image)
+        # get shape
+        w, h, d = im.shape
 
-print("pic rgb")
-print(pictureRGB)
+        xcounter = 0
+        ycounter = 0
 
-
-image = Image.open("./finalImage/face.png")
-im = np.array(image)
-# get shape
-w, h, d = im.shape
-
-xcounter = 0
-ycounter = 0
-
-endx = 7
-endy = 7
-
-tempR = 0
-tempG = 0
-tempB = 0
-
-tempCounter = 0
-
-while True:
-    pix = image.load()
-    # print(image.size)
-    # print(pix[xcounter,ycounter])
-
-    print("XCounter = " + str(xcounter))
-    print("YCounter = " + str(ycounter))
-    print("endx = " + str(endx))
-    print("endy = " + str(endy))
-
-    if ycounter == 255 and xcounter == 255:
-        #faceAverages.append([tempR/64, tempG/64, tempB/64])
-        # print("appended")
-        #tempR = 0
-        #tempB = 0
-        #tempG = 0
-        break
-    if ycounter == endy and xcounter == 255:
-        endy += 8
         endx = 7
-        ycounter += 1
-        xcounter = -1
-        print("face avg")
+        endy = 7
+
         tempR = 0
-        tempB = 0
         tempG = 0
-        # print(len(faceAverages))
-    if ycounter == endy and xcounter == endx:
-        ycounter -= 7
-        endx += 8
-        faceAverages.append([tempR/64, tempG/64, tempB/64])
-        print("appended")
-        tempR = 0
         tempB = 0
-        tempG = 0
-    if xcounter == endx:
-        xcounter -= 8
-        ycounter += 1
 
-    tempR += pix[xcounter, ycounter][0]
-    tempG += pix[xcounter, ycounter][1]
-    tempB += pix[xcounter, ycounter][2]
+        tempCounter = 0
 
-   # print("special pix")
-    #print(pix[125, 10])
-    #print("R = " + str(tempR))
-    #print("G = " + str(tempG))
-    #print("B = " + str(tempB))
+        while True:
+            pix = larger_image.load()
 
-    xcounter += 1
+            if ycounter == 255 and xcounter == 255:
+                break
+            if ycounter == endy and xcounter == 255:
+                endy += 8
+                endx = 7
+                ycounter += 1
+                xcounter = -1
+                tempR = 0
+                tempB = 0
+                tempG = 0
+            if ycounter == endy and xcounter == endx:
+                ycounter -= 7
+                endx += 8
+                faceAverages.append([tempR/64, tempG/64, tempB/64])
+                tempR = 0
+                tempB = 0
+                tempG = 0
+            if xcounter == endx:
+                xcounter -= 8
+                ycounter += 1
 
-print(faceAverages)
+            tempR += pix[xcounter, ycounter][0]
+            tempG += pix[xcounter, ycounter][1]
+            tempB += pix[xcounter, ycounter][2]
 
+            xcounter += 1
 
-#print("len 1 = " + str(len(faceAverages[0])))
-#print("len 2 = " + str(len(faceAverages[1])))
+        tempClosest = []
 
+        # Compare the 8x8 square colour average with the inputted image colours and determine which are closest.
+        for square in faceAverages:
+            counter = 0
+            diff = 100000
+            closest = "none"
+            for img in pictureRGB:
+                colourDistance = math.sqrt((pictureRGB[img][0]-square[0])**2+(
+                    pictureRGB[img][1]-square[1])**2+(pictureRGB[img][2]-square[2])**2)
+                if colourDistance < diff:
+                    diff = colourDistance
+                    closest = img
+                counter += 1
+                if counter == len(pictureRGB):
+                    tempClosest.append(closest)
 
-tempClosest = []
-# Compare the 8x8 square colour average with the inputted image colours and determine which are closest.
-for square in faceAverages:
-    counter = 0
-    diff = 100000
-    closest = "none"
-    for img in pictureRGB:
-        colourDistance = math.sqrt((pictureRGB[img][0]-square[0])**2+(
-            pictureRGB[img][1]-square[1])**2+(pictureRGB[img][2]-square[2])**2)
-        if colourDistance < diff:
-            diff = colourDistance
-            closest = img
-        counter += 1
-        if counter == len(pictureRGB):
-            tempClosest.append(closest)
-
-        if len(tempClosest) == 31:
-            closestImages.append(tempClosest)
-            tempClosest = []
-
-print(closestImages)
-
-
-horizontalImages = []
-counter = 0
-for i in range(31):
-    images = [Image.open(x) for x in closestImages[counter]]
-    widths, heights = zip(*(i.size for i in images))
-
-    total_width = sum(widths)
-    max_height = max(heights)
-
-    new_im = Image.new('RGB', (total_width, max_height))
-
-    x_offset = 0
-    for im in images:
-        new_im.paste(im, (x_offset, 0))
-        x_offset += im.size[0]
-
-    new_im.save('./horizontalImages/horiz' + str(counter) + '.jpg')
-    horizontalImages.append('horiz' + str(counter) + '.jpg')
-
-    counter += 1
+                if len(tempClosest) == 31:
+                    closestImages.append(tempClosest)
+                    tempClosest = []
 
 
-print("horizontal images = ")
-print(horizontalImages)
+        horizontalImages = []
+        counter = 0
 
+        new_im = Image.new('RGB', size = larger_size)
 
-images = [Image.open("./horizontalImages/"+x) for x in horizontalImages]
-widths, heights = zip(*(i.size for i in images))
+        y_offset = 0
+            
+        # going to loop through everything, building the images
+        for i in range(31):
+            images = [Image.open(f'{path}/8images/{x}')
+                    for x in closestImages[counter]]
+            widths, heights = zip(*(i.size for i in images))
 
-total_width = max(widths)
-max_height = sum(heights)
+            total_width = sum(widths)
+            max_height = max(heights)
 
-new_im = Image.new('RGB', (total_width, max_height))
+            new_im_x = Image.new('RGB', (total_width, max_height))
 
-y_offset = 0
-for im in images:
-    new_im.paste(im, (0, y_offset))
-    y_offset += im.size[1]
+            x_offset = 0
+            for im in images:
+                new_im_x.paste(im, (x_offset, 0))
+                x_offset += im.size[0]
 
-new_im.save('./horizontalImages/final.jpg')
+            new_im.paste(new_im_x, (0, y_offset))
+            y_offset += new_im_x.size[1]
+
+            counter += 1
+
+        print("Finished!")
+        return new_im
+    return None
