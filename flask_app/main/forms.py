fb2cd404422469bc28e677b8f3fb8d0116e2e82e
ehuan2 <eric.haoran.huang@gmail.com>
2020-06-21 12:27:21 -0400

    No pngs!

--- flask_app/main/forms.py
+++ flask_app/main/forms.py
@@ -3,9 +3,9 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms import SubmitField
 
 class ImageInputForm(FlaskForm):
-    image = FileField("Upload PNG or JPG Image", validators = [FileAllowed(['jpg', 'png'])])
+    image = FileField("Upload JPG Image", validators = [FileAllowed(['jpg'])])
     submit = SubmitField("Upload Image")
 
 class LargerImageForm(FlaskForm):
-    image = FileField("Upload the Larger PNG or JPG Image", validators = [FileAllowed(['jpg', 'png'])])
+    image = FileField("Upload the Larger JPG Image", validators = [FileAllowed(['jpg'])])
     submit = SubmitField("Create Image")
